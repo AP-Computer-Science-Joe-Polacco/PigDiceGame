db8a0e43cd6f9ecb67482d57a5c931a6831aa537
jpolacco <jpolacco@gmail.com>
2020-09-13 13:16:17 -0700

    Add files via upload

---
+++ PigDiceGame.java
@@ -0,0 +1,59 @@
+
+/**
+ * The game of Pig is a simple two-player dice game in which the first player to
+ * reach 100 or more points wins. Players take turns. On each turn, a player rolls a
+ * six-sided die:
+ * 1. If the player rolls a 1, then the player gets no new points and it becomes the other
+ *    player’s turn.
+ * 2. If the player rolls 2 through 6, then he or she can either ROLL AGAIN or
+ *    HOLD. At this point, the sum of all rolls is added to the player’s score, and it
+ *    becomes the other player’s turn.
+ *    
+ * Write a program that plays the game of Pig, where one player is a human and the
+ * other is the computer. When it is the human’s turn, the program should show the
+ * score of both players and the previous roll. Allow the human to input “r” to roll
+ * again or “h” to hold.
+ * 
+ * The computer program should play according to the following rule:
+ * 
+ * Keep rolling when it is the computer’s turn until it has accumulated 20 or
+ * more points, then hold. If the computer wins or rolls a 1, then the turn ends
+ * immediately.
+ * 
+ * Allow the human to roll first.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PigDiceGame
+{
+    
+    public static void main(String[] args){
+        //TODO
+        
+    }
+    
+    /*
+     * Returns a random integer between 1 and 6 inclusive
+     */
+    public static int roll(){
+        //TODO
+        return 1; // delete this line of code
+    }
+    
+    // You can write additional static methods, but it's not required.
+    // Static methods are class methods. That is, they don't belong to a specific object or instance.
+    // Note: this program is an example of top-down programming. It's not an object-oriented approach.
+    // 
+    // Later this semester, we might write this program by thinking 
+    // about the pieces(objects) that make it up. For example, we might want a Dice object,
+    // a Player object, a ScoreBoard object, and a PigGame object. 
+    // This is an object-oriented approach (also called bottom-up programming). 
+
+        
+        
+        
+    
+    
+    
+}
